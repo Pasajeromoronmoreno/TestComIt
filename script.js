f2d1c28979a59022ad0802dac691234541b9e777
Moises Rivas <mardemza@hotmail.com>
2016-08-09 21:24:47 -0300

    Sitio default con carga asincronica

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-(function(){
-    console.log("Prueba")
-})();
+$(function(){
+    
+
+    // -- Cargar menu
+    $.ajax("/header.html").done(function(result){
+        $("#menu").html(result);
+    });
+
+    // -- Cargar footer
+    $.ajax("/footer.html").done(function(result){
+        $("#footer").html(result);
+    });
+
+    // -- Cargar pagina 1
+    $.ajax("/pages/page1.html").done(function(result){
+        $("#content-principal").html(result);
+    });
+
+    // -- Carga cuando se hace click en algun item del menu
+    $(document).on("click","#menu-principal li > a",function(e){
+        e.preventDefault();
+        var url = $(this).attr("href");
+
+        // -- Cargo el contenido de la pagina en el div principal
+        $.ajax(url).done(function(result){
+            $("#content-principal").html(result);
+        });
+    });
+});
